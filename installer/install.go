5d6cffe45554c1779fca0b8ae432ca92b133cac9
Shivam Mukhade <smukhade@redhat.com>
2022-08-14 14:10:26 +0530

    adds installer package
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

---
+++ installer/install.go
@@ -0,0 +1,64 @@
+package installer
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+type Installer struct {
+	diskTargetLocation string
+}
+
+func NewInstaller() *Installer {
+	return &Installer{
+		diskTargetLocation: "",
+	}
+}
+
+func (i *Installer) Handler() http.HandlerFunc {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		// TODO: validate input
+
+		go i.installPackage("install")
+
+		fmt.Fprint(response, "request received!")
+	})
+}
+
+func (i *Installer) installPackage(pkg string) {
+	venv := "venv"
+	defer cleanup(venv)
+
+	// create venv
+	cmd, err := exec.Command("/bin/sh", "installer/create.sh", venv).Output()
+	if err != nil {
+		fmt.Println("failed to create venv: ", err)
+		return
+	}
+	fmt.Println("create output: ", string(cmd))
+
+	// install package
+	targetDir := pkg
+	cmd, err = exec.Command("/bin/sh", "installer/install.sh", venv, targetDir, pkg).Output()
+	if err != nil {
+		fmt.Println("failed to install package: ", err)
+		return
+	}
+	fmt.Println("install output: ", string(cmd))
+
+	// zip and copy package
+	cmd, err = exec.Command("/bin/sh", "installer/copy.sh", venv, targetDir, fmt.Sprint(pkg+".zip")).Output()
+	if err != nil {
+		fmt.Println("failed to zip and copy package: ", err, string(cmd))
+		return
+	}
+	fmt.Println("copy output: ", string(cmd))
+}
+
+func cleanup(venv string) {
+	cmd, err := exec.Command("/bin/sh", "installer/cleanup.sh", venv).Output()
+	if err == nil {
+		fmt.Println(string(cmd))
+	}
+}
--- main.go
+++ main.go
@@ -1,60 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
+
+	"github.com/sm43/python-package-installer/installer"
 )
 
 func main() {
 	log.Println("Starting Server...")
 
-	http.HandleFunc("/", installHandler())
+	installer := installer.NewInstaller()
+	http.HandleFunc("/", installer.Handler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func installHandler() http.HandlerFunc {
-	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		// TODO: validate input
-
-		installPackage("install")
-
-		fmt.Fprint(response, "package installed!")
-	})
-}
-
-func installPackage(pkg string) {
-	venv := "venv"
-	defer cleanup(venv)
-
-	// create venv
-	cmd, err := exec.Command("/bin/sh", "installer/create.sh", venv).Output()
-	if err != nil {
-		fmt.Println("failed to create venv: ", err)
-	}
-	fmt.Println("create output: ", string(cmd))
-
-	// install package
-	targetDir := pkg
-	cmd, err = exec.Command("/bin/sh", "installer/install.sh", venv, targetDir, pkg).Output()
-	if err != nil {
-		fmt.Println("failed to install package: ", err)
-	}
-	fmt.Println("install output: ", string(cmd))
-
-	// zip and copy package
-	cmd, err = exec.Command("/bin/sh", "installer/copy.sh", venv, targetDir, fmt.Sprint(pkg+".zip")).Output()
-	if err != nil {
-		fmt.Println("failed to zip and copy package: ", err, string(cmd))
-	}
-	fmt.Println("copy output: ", string(cmd))
-}
-
-func cleanup(venv string) {
-	cmd, err := exec.Command("/bin/sh", "installer/cleanup.sh", venv).Output()
-	if err == nil {
-		fmt.Println(string(cmd))
-	}
 }
